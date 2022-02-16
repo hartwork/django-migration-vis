3293345df5cbb06384a86abfe47b2911a438e2d9
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2022-02-16 09:17:29 +0300

    Drop support for EOL Python versions

--- setup.py
+++ setup.py
@@ -37,8 +37,6 @@ if __name__ == '__main__':
                 'License :: OSI Approved :: MIT License',
                 'Natural Language :: English',
                 'Programming Language :: Python',
-                'Programming Language :: Python :: 3.5',
-                'Programming Language :: Python :: 3.6',
                 'Programming Language :: Python :: 3.7',
                 'Programming Language :: Python :: 3.8',
             ],
@@ -46,5 +44,5 @@ if __name__ == '__main__':
                 'Django',
                 'graphviz',
             ],
-            python_requires='>=3.5',
+            python_requires='>=3.7',
     )
