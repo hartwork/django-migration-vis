99eb0764f9bee46f81b97c0d0bfe7bc7251bcf67
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2022-03-08 15:11:39 +0300

    Add support for latest Python versions

--- setup.py
+++ setup.py
@@ -37,6 +37,8 @@ if __name__ == '__main__':
             'Programming Language :: Python',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
         ],
         install_requires=[
             'Django>=2.2',
